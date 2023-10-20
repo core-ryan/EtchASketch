c1b1587999264f5669758455d43c98a716a78ee1
core-ryan <ryanjr0620@yahoo.com>
2023-10-20 00:22:49 -0600

    - Added clearGrid function to erase all cells in clearGrid - Added createGridWithInput to create new grid using input from user (number of cells) using original gridspace - Added event listener for "New Sketch Pad" button to get input from user, clear existing grid, and call function to make new grid

--- script.js
+++ script.js
@@ -26,6 +26,51 @@ function createGrid(){
     }
 }
 
+function clearGrid(){
+    // Removes all content from grid-container div
+    document.getElementById("grid-container").innerHTML = "";
+}
 
+function createGridWithInput(numOfSquares){
+    // Will be called when user presses "New Sketch Pad" button and provides input.
+    
+    for(let i = 0; i < numOfSquares; i++){
+        // Create new column div to hold cells. Add "column" class
+        let columnDiv = document.createElement("div");
+        columnDiv.classList.add("column");
+
+        for(let j = 0; j < numOfSquares; j++){
+            // Create new cell. Add "cell" class
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("cell");
+
+            // NEED TO OVERWRITE CELL SIZE
+            let x = 1200/numOfSquares + "px";
+            newDiv.style.width = x;
+            newDiv.style.height = x;
+
+            // Event listener to change cell color when mouse is over cell
+            newDiv.addEventListener("mouseover", function(){
+                newDiv.style.backgroundColor = "blue";
+            })
+
+            // Add cell to column
+            columnDiv.appendChild(newDiv);
+        }
+
+        // Add column to main grid container div
+        document.getElementById("grid-container").appendChild(columnDiv);
+
+    }
+}
 
 createGrid();
+
+let sketchButton = document.getElementById("sketch-button");
+sketchButton.addEventListener("click", function(){
+    let userResponse = prompt("How many squares per side?");
+    console.log(1200 / userResponse);
+    clearGrid();
+    createGridWithInput(userResponse);
+})
+
